429601d6af782e3f3c64f43cd7395bd1d9b25503
Fael Br Gamer <rafael.alves0_1@hotmail.com>
2025-07-24 16:22:51 -0300

    adding testimonial page view with default values

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,7 +98,7 @@ modalCloses.forEach((modalClose) => {
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
       cssMode: true,
       loop: true,
       navigation: {
@@ -113,6 +113,23 @@ let swiper = new Swiper(".portfolio__container", {
 
 /*==================== TESTIMONIAL ====================*/
 
+let swiperTestimonial = new Swiper(".testimonial__container", {
+      loop: true,
+      grabcursor: true,
+      spacebetween: 48,
+
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+      },
+
+      breakpoints: {
+        568: {
+          slidesPerView: 2,
+        },
+      },
+    });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
